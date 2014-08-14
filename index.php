7e2d709b64e511654ef97f31c1f1acf53aeadbf4
Andres V <darklink898@gmail.com>
2014-08-14 08:04:38 -0600

    index only, jenkins now handles the deployment

--- index.php
+++ index.php
@@ -8,46 +8,41 @@ function getdirsbydate($dir) {
     asort($tmp);
     return array_reverse($tmp);
 }
-function getlog() {
-    $lines = file('debug.log');
-    $ret = array();
-    foreach ($lines as $line) {
-       $line = trim($line);
-       if (!$line || $line[0]==='=') continue;
-       if (strpos($line,') 2>&1')!==false) $line = substr($line,strpos($line,'&&')+2,strpos($line,') 2>&1')-strlen($line));
-       $ret[] = trim($line);
-    }
-    return $ret;
-}
 
 $apps = getdirsbydate('/var/lib/tomcat7/webapps');
-$log = getlog();
 ?>
 <html>
 <head>
 <title>Falafel Status</title>
 <style>
-body,html{margin:0;padding:0; background: #222;font-family: "Droid Sans","Helvetica",sans-serif;}
-#wrap{max-width:1200px;min-width:800px;margin: 0 auto;color: #7af;}
-h1,h2,h3,h4,h5,h6{margin: .5em 0; font-weight: bold;}
+body,html{margin:0;padding:0;background-image:url('bg.jpg');font-family: "Droid Sans","Helvetica Neue","Helvetica",sans-serif;}
+body{padding:2em;}
+#wrap{max-width:800px;background:#e0e0e0;background:rgba(255,240,255,.9);margin: 0 auto;color: #333;padding:.25em .5em;}
+h1,h2,h3,h4,h5,h6{margin: .2em 1rem; font-weight: bold;}
+h1{float:left;font-size:1.4em;}
 h3{opacity:.5;}
-#apps{width:40%;float:left;}
-#log{width:59%;float:right;}
-#log-container{background:#1a1a1a;font-size:13px;font-family:"Liberation Mono","Consolas",monospace;overflow-y:scroll;overflow-x:hidden;height:600px;}
-.line{padding:.25em .5em; border-bottom: #2a2a2a 1px dashed;}
-.app{display:block; border-top: #469 1px dashed; padding: .5em 0;}
-a:link,a:visited{text-decoration: none;color: #7af;}
-a:hover{color: #9cf;background: #333;}
-#dashboard-l{border: #555 4px dashed;padding:4px;float:right;}
+#apps{clear:both;}
+.app{display:block; border-top: #ccc 1px solid; padding: 1em;}
+a:link,a:visited{transition:all .3s;text-decoration: none;color: #38c;}
+a:hover{color: #49d;background: #e8e8e8;background:rgba(255,240,255,.5);}
+.app p{margin: 0;margin-top:.6em;}
+.app h4 {margin: 0}
+nav{float:right;font-size:1.4em;font-weight:bold;}
+nav a{font-size:.7em;margin-left:4px;}
+@media (max-width:420px) {body{padding:0;}}
 </style>
 </head>
 <body>
 <div id="wrap">
-<h1>Falafel Status
-<a href="dash/" id="dashboard-l">Dashboard</a>
-</h1>
+<div id="header">
+<h1>Falafel Status</h1>
+<nav>
+<a href="http://falafel.villarreal.co.cr:8000/">Jenkins</a>
+<a href="dash/">Dashboard</a>
+</nav>
+</div>
 <div id="apps">
-<h3>Branches</h3>
+<h3>Deployments</h3>
 <?php foreach ($apps as $app => $time): ?>
 <a href="http://falafel.villarreal.co.cr:8080/<?php echo $app; ?>" class="app">
     <h4><?php echo $app; ?></h4>
@@ -55,17 +50,6 @@ a:hover{color: #9cf;background: #333;}
 </a>
 <?php endforeach; ?>
 </div>
-<div id="log">
-<h3>Command Log</h3>
-<div id="log-container">
-<?php foreach ($log as $l): ?>
-<div class="line"><?php echo $l; ?></div>
-<?php endforeach; ?>
 </div>
-</div>
-</div>
-<script>
-var s=document.getElementById('log-container');s.scrollTop=s.scrollHeight;
-</script>
 </body>
 </html>
